09a24c9ee9a2059b8d012cf6c74c2ce53004306d
kk98033 <a8829037@gmail.com>
2024-04-14 15:52:04 +0800

    cleaner unity scripts

---
+++ unity/TTS_API.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+using UnityEngine.Networking;
+
+public class TTS_API : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    void Start()
+    {
+    }
+
+    public void callTTSandPlay(string text)
+    {
+        StartCoroutine(StreamAudioFromAPI($"http://127.0.0.1:9880/?text={text}&text_language=zh"));
+    }
+
+    IEnumerator StreamAudioFromAPI(string uri)
+    {
+        using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(uri, AudioType.OGGVORBIS))
+        {
+            ((DownloadHandlerAudioClip)www.downloadHandler).streamAudio = true;
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                AudioClip clip = DownloadHandlerAudioClip.GetContent(www);
+                audioSource.clip = clip;
+                audioSource.Play();
+            }
+        }
+    }
+}

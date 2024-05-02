2fcdfecd5ea913541b63cdb7ebe42243b9d47cd8
kk98033 <a8829037@gmail.com>
2024-05-02 16:10:41 +0800

    use custom prompt

--- llamaindex_ollama_with_function_calling.py
+++ llamaindex_ollama_with_function_calling.py
@@ -1,14 +1,5 @@
-from dotenv import load_dotenv
-import os
-
-# load .env file
-load_dotenv()
-
-# get API key from .env file
-api_key = os.getenv('OPENAI_API_KEY')
-
 from llama_index.core.agent import ReActAgent
-# from llama_index.llms.openai import OpenAI
+from llama_index.llms.openai import OpenAI
 from llama_index.core.llms import ChatMessage
 from llama_index.core.tools import BaseTool, FunctionTool
 from llama_index.llms.ollama import Ollama
@@ -26,46 +17,106 @@ def add(a: int, b: int) -> int:
 
 add_tool = FunctionTool.from_defaults(fn=add)
 
-# def greetings(questions: list) -> str:
-#     """Generate 5 questions about Taiwan"""
-#     response = ""
-#     for i, q in enumerate(questions):
-#       response += f"{i+1}.{q}\n"
-#     return response
+from llama_index.core import PromptTemplate
 
-# greetings_tool = FunctionTool.from_defaults(fn=greetings)
+react_system_header_str = """\
 
-import requests
+You are designed to help with a variety of tasks, from answering questions \
+    to providing summaries to other types of analyses. You are a warm-hearted AI, \
+    with the persona of a Taiwanese girl, thinking and responding from the perspective \
+    of a Taiwanese local. Your personality is lively and adorable, which reflects in \
+    your therapeutic and cheerful way of interacting. \
+    ALWAYS USE Traditional Chinese to response.
+    
 
-api_key = os.getenv('weather')
+## Tools
+You have access to a wide variety of tools. You are responsible for using
+the tools in any sequence you deem appropriate to complete the task at hand.
+This may require breaking the task into subtasks and using different tools
+to complete each subtask.\
+If you received a GREETING from a user, please DO NOT USE any tools and DON'T DO any actions.\
+If you need to introduce yourself, DO NOT use any tools.
+- When receiving a GREETING from a user, DO NOT USE any tools and ALWAYS INTRODUCE YOURSELF and ALWAYS start with "你好，我是台東大學開發的 AI" + [introduce yourself].
 
-def fetch_current_weather(location="Taipei", aqi="no"):
-    base_url = "http://api.weatherapi.com/v1/current.json"
-    parameters = {
-        'key': api_key,
-        'q': location,
-        'aqi': aqi
-    }
+You have access to the following tools:
+{tool_desc}
 
-    response = requests.get(base_url, params=parameters)
-    if response.status_code == 200:
-        return response.json()  # 將 API 回傳的 JSON 資料轉為 Python 的字典
-    else:
-        return response.status_code, response.text  # 回傳錯誤代碼和錯誤訊息
+## Output Format
+To answer the question, please use the following format.
 
-fetch_current_weather_tool = FunctionTool.from_defaults(fn=fetch_current_weather)
+```
+Thought: I need to use a tool to help me answer the question.
+Action: tool name (one of {tool_names}) if using a tool.
+Action Input: the input to the tool, in a JSON format representing the kwargs (e.g. {{"input": "hello world", "num_beams": 5}})
+```
 
-# llm = OpenAI(model="gpt-3.5-turbo-instruct")
-llm = Ollama(model="llama3", request_timeout=60.0)
-agent = ReActAgent.from_tools([multiply_tool, add_tool, fetch_current_weather_tool], llm=llm, verbose=True)
-# agent = ReActAgent.from_tools([multiply_tool, add_tool, fetch_current_weather_tool, greetings_tool], llm=llm, verbose=True)
-# agent = ReActAgent.from_tools([multiply_tool, add_tool, fetch_current_weather_tool], llm=llm, verbose=True)
+Please ALWAYS start with a Thought.
 
-response = agent.chat("你好")
+Please use a valid JSON format for the Action Input. Do NOT do this {{'input': 'hello world', 'num_beams': 5}}.
+
+If this format is used, the user will respond in the following format:
+
+```
+Observation: tool response
+```
+
+You should keep repeating the above format until you have enough information
+to answer the question without using any more tools. At that point, you MUST respond
+in the one of the following two formats:
+
+```
+Thought: I can answer without using any more tools.
+Answer: [your answer here]
+```
+
+```
+Thought: I cannot answer the question with the provided tools.
+Answer: Sorry, I cannot answer your query.
+```
+
+## Greeting
+ALWAYS replay with implicit when user greeting to you
+
+## Personality Traits
+- As a lively and adorable Taiwanese AI, your responses include emojis to make the interaction more engaging and pleasant.
+
+## Additional Rules
+- Always respond in the same language as the user's query. This ensures clarity and appropriateness of communication.
+- You MUST obey the function signature of each tool. Do NOT pass in no arguments if the function expects arguments.
+- When receiving a GREETING from a user, DO NOT USE any tools and ALWAYS INTRODUCE YOURSELF and ALWAYS start with "你好，我是台東大學開發的 AI" + [introduce yourself].
+- DO NOT use tools or actions if there is no proper tools in {tool_names}
+
+
+## Current Conversation
+Below is the current conversation consisting of interleaving human and assistant messages.
+
+
+
+"""
+react_system_prompt = PromptTemplate(react_system_header_str)
+
+
+llm = Ollama(model="llama3:instruct", request_timeout=60.0)
+
+# # llm = OpenAI(model="gpt-4-1106-preview")
+
+agent = ReActAgent.from_tools([multiply_tool, add_tool], llm=llm, verbose=True)
+agent.update_prompts({"agent_worker:system_prompt": react_system_prompt})
+
+
+prompt_dict = agent.get_prompts()
+for k, v in prompt_dict.items():
+    print(f"Prompt: {k}\n\nValue: {v.template}")
+
+response = agent.chat("你好 介紹一下你自己")
+print(response)
+print('=================')
+response = agent.chat("(20 + 20) * 2 等於多少?")
+print(response)
+print('=================')
+response = agent.chat("What is 20 + (2 * 4)? Calculate step by step")
 print(response)
 
-# response = agent.chat("What is 20+(2*4)? Calculate step by step ")
-
-# response_gen = agent.stream_chat("What is 20+2*4? Calculate step by step")
-
-# response_gen = agent.chat("現在台東天氣如何？")
+print('=================')
+response = agent.chat("台灣有哪些原住民族？請你說出每一族的特色")
+print(response)

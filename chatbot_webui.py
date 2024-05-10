484bca7444f1b1498158f589a95fee4764e02211
kk98033 <a8829037@gmail.com>
2024-05-10 14:03:44 +0800

    modify layout

--- chatbot_webui.py
+++ chatbot_webui.py
@@ -24,30 +24,42 @@ def call_api(message):
     except requests.RequestException as e:
         return f"API Error: {str(e)}"
 
-st.title('NTTU 原住民老師')
-
-# Input box and send button
-user_input = st.text_input("Please enter your message:", key="user_input")
-
-# Add sidebar with image and text
-st.sidebar.image("llama_logo.png", use_column_width=True) 
-st.sidebar.write("NTTU 原住民老師")
-
-
-if st.button("Send"):
+def send_message():
+    user_input = st.session_state.user_input  
     if user_input:
-        api_response = call_api(user_input)
-        st.session_state.input = ""
+        api_response = call_api(user_input)  # 調用 API，此處需自定義函式
         st.session_state.responses.append({"user": "user", "text": user_input})
         st.session_state.responses.append({"user": "bot", "text": api_response})
+        st.session_state.user_input = ""  
 
-# Display the chat history
-for message in st.session_state.responses:
-    if message['user'] == "user":
-        with st.container():
-            st.markdown(f"<div style='text-align: right;'><i class='fa fa-user icon'></i></div>", unsafe_allow_html=True)
-            st.markdown(f"<div class='message user'>{message['text']}</div>", unsafe_allow_html=True)
-    else:
-        with st.container():
-            st.markdown(f"<div style='text-align: left;'><i class='fa fa-robot icon'></i></div>", unsafe_allow_html=True)
-            st.markdown(f"<div class='message bot'>{message['text']}</div>", unsafe_allow_html=True)
+def display_chat_history():
+    """Displays the chat history."""
+    for message in st.session_state.responses:
+        if message['user'] == "user":
+            with st.container():
+                st.markdown(f"<div style='text-align: right;'><i class='fa fa-user icon'></i></div>", unsafe_allow_html=True)
+                st.markdown(f"<div class='message user'>{message['text']}</div>", unsafe_allow_html=True)
+        else:
+            with st.container():
+                st.markdown(f"<div style='text-align: left;'><i class='fa fa-robot icon'></i></div>", unsafe_allow_html=True)
+                st.markdown(f"<div class='message bot'>{message['text']}</div>", unsafe_allow_html=True)
+
+# Title Container
+title_container = st.container()
+title_container.title('NTTU 原住民老師')
+
+# Main Chat Container
+chat_container = st.container()
+with chat_container:
+    display_chat_history()
+
+# Input Container
+input_container = st.container()
+with input_container:
+    user_input = st.text_input("Please enter your message:", key="user_input", on_change=send_message, placeholder="Type a message and press enter")
+    if st.button("Send"):
+        send_message()
+
+# # Add sidebar with image and text
+st.sidebar.image("llama_logo.png", use_column_width=True) 
+st.sidebar.write("NTTU 原住民老師")

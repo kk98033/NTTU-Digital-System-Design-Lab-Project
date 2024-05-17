690da5a49a86625e3084e02153c2caadfc2edba7
kk98033 <a8829037@gmail.com>
2024-05-17 13:58:58 +0800

    add some comment

--- chatbot_webui.py
+++ chatbot_webui.py
@@ -50,11 +50,13 @@ def send_message():
             st.markdown(f"<div style='text-align: left;'><i class='fa fa-robot icon'></i></div>", unsafe_allow_html=True)
 
         # 為了可以 stream response
+        # 下面註解的這行是可以讓她文字有背景框框，但是這樣就無法用 MD 格式顯示(沒有程式碼框框等功能...)
         # bot_message_container.markdown(f"<div class='message bot'>{""}", unsafe_allow_html=True)
         for api_response in call_api(user_input):
             # response_text += f'{api_response}<br>'
             response_text += f'{api_response}\n'
             # 每次更新回應時更新容器內容
+            # 下面註解的這行是可以讓她文字有背景框框，但是這樣就無法用 MD 格式顯示(沒有程式碼框框等功能...)
             # bot_message_container.markdown(f"<div class='message bot'>{response_text}</div>", unsafe_allow_html=True)
             bot_message_container.markdown(response_text)
 
@@ -84,6 +86,7 @@ def display_chat_history(message_display_area):
                 st.markdown(f"<div class='message user'>{message['text']}</div>", unsafe_allow_html=True)
             else:
                 st.markdown(f"<div style='text-align: left;'><i class='fa fa-robot icon'></i></div>", unsafe_allow_html=True)
+                # 下面註解的這行是可以讓她文字有背景框框，但是這樣就無法用 MD 格式顯示(沒有程式碼框框等功能...)
                 # st.markdown(f"<div class='message bot'>{message['text']}</div>", unsafe_allow_html=True)
                 st.markdown(message['text'])
 # Title Container
